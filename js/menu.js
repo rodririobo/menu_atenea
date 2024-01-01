8bae8bc1aadd281f800e163b2665ef75fef2492e
Roberto <rodri.riobo@gmail.com>
2024-01-01 21:08:04 +0100

    update

--- js/menu.js
+++ js/menu.js
@@ -56,7 +56,7 @@ function obtenerContenido() {
       }else{ 
        
             listItem.textContent = textoSinComillas;
-    lista.appendChild(listItem);
+    		lista.appendChild(listItem);
               
              
             }			
